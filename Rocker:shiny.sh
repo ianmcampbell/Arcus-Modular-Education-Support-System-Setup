c3c34a1648294dc59f690669858685ef3a95626f
Ian Campbell <ianmcampbell@gmail.com>
2019-11-19 00:11:49 -0500

    Add documentation and PackageManagement

--- Rocker:shiny.sh
+++ Rocker:shiny.sh
@@ -1,5 +1,6 @@
 FROM rocker/shiny-verse
 
+#Install linux software
 RUN apt-get update -qq && apt-get -y --no-install-recommends install \
    openjdk-8-jdk \
    liblzma-dev \
@@ -12,10 +13,13 @@ RUN apt-get update -qq && apt-get -y --no-install-recommends install \
    git \
    nano
    
+#Prepare Java Path for rJava install for RJDBC    
 ENV LD_LIBRARY_PATH=/usr/lib/jvm/jre/lib/amd64:/usr/lib/jvm/jre/lib/amd64/default
 
+#Prepair rJava install
 RUN R CMD javareconf
 
+#Install packages
 RUN R -e "install.packages('RCurl')"
 RUN R -e "install.packages('learnr')"
 RUN R -e "install.packages('RJDBC')"
@@ -25,35 +29,60 @@ RUN R -e "install.packages('coin')"
 RUN R -e "install.packages('FSA')"
 RUN R -e "install.packages('rcompanion')"
 
+#Remove default example apps
 RUN cd /srv/shiny-server/
 RUN rm -rf /srv/shiny-server/*
 RUN chown shiny:shiny /srv/shiny-server/
 
+#Finish setup as shiny user
 USER shiny
 
+#Add github.com known hosts
 RUN mkdir ~/.ssh/
 RUN ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-RUN git clone git@github.com:braunsb/Lessons.git
-RUN ln -s /srv/shiny-server/Lessons/* /srv/shiny-server
-RUN git clone git@github.com:ianmcampbell/ModuleTable.git
+#Import SSH private key
+ADD --chown=shiny:shiny id_rsa /home/shiny/.ssh/id_rsa
 
+#Set permissions
+RUN chmod 700 /home/shiny/.ssh/id_rsa
+
+#Clone Lesson and ModuleTable repos
+RUN cd /srv/shiny-server/
+RUN git clone git@github.com:braunsb/Lessons.git /srv/shiny-server/Lessons/
+#Symbolically link apps into top level directory for service
+RUN ln -s /srv/shiny-server/Lessons/* /srv/shiny-server
+RUN git clone git@github.com:ianmcampbell/ModuleTable.git /srv/shiny-server/ModuleTable/
+
+#Download other apps directly from repo
 RUN curl https://codeload.github.com/ianmcampbell/Arcus-Modular-Education-Support-System/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Arcus-Modular-Education-Support-System-master/Personalized-Learning-Plan
 RUN curl https://codeload.github.com/ianmcampbell/Arcus-Modular-Education-Support-System/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Arcus-Modular-Education-Support-System-master/Lesson-Generator
 RUN curl https://codeload.github.com/ianmcampbell/Arcus-Modular-Education-Support-System/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Arcus-Modular-Education-Support-System-master/list
 RUN curl https://codeload.github.com/ianmcampbell/Arcus-Modular-Education-Support-System/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Arcus-Modular-Education-Support-System-master/TableUpdater
+RUN curl https://codeload.github.com/ianmcampbell/Arcus-Modular-Education-Support-System/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Arcus-Modular-Education-Support-System-master/PackageManagement
+RUN curl https://codeload.github.com/ianmcampbell/Curricula/tar.gz/master | tar -C /srv/shiny-server/ -xz --strip=1 Curricula-master/CurriculaTable.csv
 
+#Import curricula
 RUN mkdir /srv/shiny-server/Personalized-Learning-Plan/curricula/
-
 RUN curl https://codeload.github.com/ianmcampbell/Curricula/tar.gz/master | tar -C /srv/shiny-server/Personalized-Learning-Plan/curricula/ -xz --strip=1 Curricula-master/
 
+#Set up Final Symlinks
+RUN ln -s /srv/shiny-server/CurriculaTable.csv /srv/shiny-server/Personalized-Learning-Plan/CurriculaTable.csv
+RUN ln -s /srv/shiny-server/CurriculaTable.csv /srv/shiny-server/Lesson-Generator/CurriculaTable.csv
+RUN ln -s /srv/shiny-server/CurriculaTable.csv /srv/shiny-server/list/CurriculaTable.csv
+RUN ln -s /srv/shiny-server/ModuleTable/ModuleTable.csv /srv/shiny-server/Personalized-Learning-Plan/ModuleTable.csv
+RUN ln -s /srv/shiny-server/ModuleTable/ModuleTable.csv /srv/shiny-server/Lesson-Generator/ModuleTable.csv
+RUN ln -s /srv/shiny-server/ModuleTable/ModuleTable.csv /srv/shiny-server/list/ModuleTable.csv
+
+#Expose default shiny-server port 
 EXPOSE 3838
 
+#Set entry for container as shiny-server binary
 CMD ["/usr/bin/shiny-server.sh"]
 
-#FROM rocker/shiny-verse/a-mess
-#EXPOSE 3838
-#CMD ["/usr/bin/shiny-server.sh"]
-
-#docker run -d -p 3839:3838
+#Use this command to run
+#docker save <image> | bzip2 > rocker-verse-amess.tar.bz2
+#bzip2 -d rocker-verse-amess.tar.gz
+#docker load -i rocker-verse-amess.tar
+#docker run -d -p 3838:3838 <image>
 
